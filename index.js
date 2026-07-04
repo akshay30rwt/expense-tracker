32b08d04e39c4f123eacebc2289f0cbc5b4d62e5
akshay30rwt <akshayrwt8700@gmail.com>
2026-07-04 21:53:19 +0530

    feat: add errorHandler in index.js

--- index.js
+++ index.js
@@ -3,12 +3,14 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 const expenseRoutes = require('./routes/expenseRoutes');
+const errorHandler = require('./middleware/errorHandler');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use('/expenses', expenseRoutes);
+app.use(errorHandler)
 
 mongoose.connect(process.env.MONGO_URI).then(() => {
     console.log('Connected to MongoDB');
